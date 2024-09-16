8a9b6c654b3ddf5f99ef3b5b1a74b6e0a054bf3d
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-17 00:13:08 +0600

    feat: prepare DTOs for create, update and read

---
+++ src/RentService/DTOs/CreateRentDto.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentService.DTOs;
+
+public class CreateRentDto
+{
+    [Required]
+    public string Address { get; set; }
+    [Required]
+    public int Beds { get; set; }
+    [Required]
+    public int Baths { get; set; }
+    [Required]
+    public int Balconies { get; set; }
+    [Required]
+    public int HouseSize { get; set; }
+    [Required]
+    public string HouseSizeUnit { get; set; }
+    [Required]
+    public int LandSize { get; set; }
+    [Required]
+    public string LandSizeUnit { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public DateTime AvailableFrom { get; set; }
+    [Required]
+    public string ImageUrl { get; set; }
+    [Required]
+    public int ReservedPrice { get; set; }
+    [Required]
+    public DateTime ConfirmBy { get; set; }
+}
