cc3c084b04836ebd8c94a900d9202470422b29f2
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-17 02:56:42 +0600

    feat: ad POST request for creation of Rent entity

--- src/RentService/Controllers/RentsController.cs
+++ src/RentService/Controllers/RentsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RentService.Data;
 using RentService.DTOs;
+using RentService.Entities;
 
 namespace RentService.Controllers;
 
@@ -47,5 +48,27 @@ public class RentsController : ControllerBase
 
         // Return the retrieved rent mapped according to the RentDto
         return _mapper.Map<RentDto>(rent);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<RentDto>> CreateRent(CreateRentDto rentDto)
+    {
+        // Prepare AutoMapper from rentDto to rent
+        var rent = _mapper.Map<Rent>(rentDto);
+
+        // TODO: Add current user as creator
+        rent.CreatedBy = "Fuad";
+
+        // Save rent to be created to memory
+        _context.Rents.Add(rent);
+
+        // Save changes from memory to the database
+        var result = await _context.SaveChangesAsync();
+
+        // If no changes were applied, the request has failed
+        if (result == 0) return BadRequest("Could not save changes to the DB");
+
+        // Return reference to GetRentById to assist in finding the created data from the database
+        return CreatedAtAction(nameof(GetRentById), new { rent.Id }, _mapper.Map<RentDto>(rent));
     }
 }
