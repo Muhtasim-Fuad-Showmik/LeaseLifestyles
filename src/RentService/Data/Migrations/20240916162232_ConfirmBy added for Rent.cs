e25640cdf64c22eee011e6b4cf19563295a90276
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-16 22:28:07 +0600

    feat: update Rent Entity to have a "ConfirmBy" deadline date

---
+++ src/RentService/Data/Migrations/20240916162232_ConfirmBy added for Rent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfirmByaddedforRent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ConfirmedAt",
+                table: "Rents",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConfirmBy",
+                table: "Rents",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmBy",
+                table: "Rents");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ConfirmedAt",
+                table: "Rents",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
