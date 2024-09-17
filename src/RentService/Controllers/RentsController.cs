bbe5e46b16339f38b2d41b0cc6276dc9f1ae9c6d
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-18 01:01:45 +0600

    feat: crete API for updating a rent record

--- src/RentService/Controllers/RentsController.cs
+++ src/RentService/Controllers/RentsController.cs
@@ -71,4 +71,39 @@ public class RentsController : ControllerBase
         // Return reference to GetRentById to assist in finding the created data from the database
         return CreatedAtAction(nameof(GetRentById), new { rent.Id }, _mapper.Map<RentDto>(rent));
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateRent(Guid id, UpdateRentDto updateRentDto)
+    {
+        // Retrieve the rent that is to be updated
+        var rent = await _context.Rents.Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        // Return a 404 Not Found response if rent is not found
+        if (rent == null) return NotFound();
+
+        // TODO: Check if landlord == username
+
+        // Update each field if an update for that field has been provided
+        rent.Item.Address = updateRentDto.Address ?? rent.Item.Address;
+        rent.Item.FloorNumber = updateRentDto.FloorNumber ?? rent.Item.FloorNumber;
+        rent.Item.Beds = updateRentDto.Beds ?? rent.Item.Beds;
+        rent.Item.Baths = updateRentDto.Baths ?? rent.Item.Baths;
+        rent.Item.Balconies = updateRentDto.Balconies ?? rent.Item.Balconies;
+        rent.Item.HouseSize = updateRentDto.HouseSize ?? rent.Item.HouseSize;
+        rent.Item.HouseSizeUnit = updateRentDto.HouseSizeUnit ?? rent.Item.HouseSizeUnit;
+        rent.Item.LandSize = updateRentDto.LandSize ?? rent.Item.LandSize;
+        rent.Item.LandSizeUnit = updateRentDto.LandSizeUnit ?? rent.Item.LandSizeUnit;
+        rent.Item.Description = updateRentDto.Description ?? rent.Item.Description;
+        rent.Item.AvailableFrom = updateRentDto.AvailableFrom ?? rent.Item.AvailableFrom;
+
+        // Save local changes to the database and retrieve the result
+        var result = await _context.SaveChangesAsync() > 0;
+
+        // If result is successful, return a Status 200 OK response
+        if (result) return Ok();
+
+        // If result is not successful, return a Status 400 Bad Request response
+        return BadRequest("Problem saving changes");
+    }
 }
