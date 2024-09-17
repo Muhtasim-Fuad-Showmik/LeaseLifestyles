a8fd01875c8f097a98cbe4e41aebabab2542d8d2
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-18 01:01:27 +0600

    feat: update UpdateRentDto to have optional fields for non-string fields; convert units to Unit type from string;

--- src/RentService/DTOs/UpdateRentDto.cs
+++ src/RentService/DTOs/UpdateRentDto.cs
@@ -1,18 +1,19 @@
 using System;
+using RentService.Entities;
 
 namespace RentService.DTOs;
 
 public class UpdateRentDto
 {
     public string Address { get; set; }
-    public int FloorNumber { get; set; }
-    public int Beds { get; set; }
-    public int Baths { get; set; }
-    public int Balconies { get; set; }
-    public int HouseSize { get; set; }
-    public string HouseSizeUnit { get; set; }
-    public int LandSize { get; set; }
-    public string LandSizeUnit { get; set; }
+    public int? FloorNumber { get; set; }
+    public int? Beds { get; set; }
+    public int? Baths { get; set; }
+    public int? Balconies { get; set; }
+    public int? HouseSize { get; set; }
+    public Unit? HouseSizeUnit { get; set; }
+    public int? LandSize { get; set; }
+    public Unit? LandSizeUnit { get; set; }
     public string Description { get; set; }
-    public DateTime AvailableFrom { get; set; }
+    public DateTime? AvailableFrom { get; set; }
 }
