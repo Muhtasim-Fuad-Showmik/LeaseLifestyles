b3a50e130f917bdc0ce3376ad5a098703b1a3a59
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-21 00:58:11 +0600

    feat: initialize the mongo database using the connection string and indexing on address, floor number and house size

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,5 +14,13 @@ app.UseAuthorization();
 
 // Middleware for directing the HTTP request to the correct API endpoint
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Address, KeyType.Text)
+    .Key(x => x.FloorNumber, KeyType.Text)
+    .Key(x => x.HouseSize, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
