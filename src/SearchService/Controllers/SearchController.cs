7a66181b461e95a5cb28be7080df79856ddfa313
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-24 03:19:09 +0600

    feat: add pagination to the search endpoint

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -15,10 +15,11 @@ public class SearchController : ControllerBase
     /// <param name="searchTerm">The search term to search for.</param>
     /// <returns>A list of items that match the search term.</returns>
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
+        int pageNumber = 1, int pageSize = 4)
     {
-        // Search all items in the Items collection
-        var query = DB.Find<Item>();
+        // Search all items in the Items collection with pagination
+        var query = DB.PagedSearch<Item>();
 
         // Sort the items by their addresses in ascending order
         query.Sort(x => x.Ascending(a => a.Address));
@@ -31,11 +32,20 @@ public class SearchController : ControllerBase
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        // Apply pagination configurations
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         // Execute the query and store the result
         var result = await query.ExecuteAsync();
 
-        // Return the result of the query
-        return result;
+        // Return the result of the query with pagination data
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
 
