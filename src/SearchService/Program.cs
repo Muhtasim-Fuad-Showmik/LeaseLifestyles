89e5d99f20b5d5f7360e391637ac15f2edb26bf4
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-27 00:39:07 +0600

    feat: add RentServiceHttpClient to the builder in Program.cs

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,9 +1,11 @@
 using SearchService.Data;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<RentServiceHttpClient>();
 
 var app = builder.Build();
 
