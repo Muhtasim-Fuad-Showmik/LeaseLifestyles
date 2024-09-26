42561e65449501b09e8fadda2b73da21939d92bd
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-27 00:54:44 +0600

    feat: update seed logic to fetch data from rent service and save any new updated data found

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -3,42 +3,47 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
+/// <summary>
+/// Class for initializing the database with data from the Rent service
+/// </summary>
 public class DbInitializer
 {
+    /// <summary>
+    /// Initializes the database with data from the Rent service
+    /// </summary>
+    /// <param name="app">The WebApplication object</param>
     public static async Task InitDb(WebApplication app)
     {
         // Initialize database using the connection string
         await DB.InitAsync("SearchDb", MongoClientSettings
             .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
 
-        // Index database by address, flood number and house size
+        // Index database by address, floor number and house size
         await DB.Index<Item>()
             .Key(x => x.Address, KeyType.Text)
             .Key(x => x.FloorNumber, KeyType.Text)
             .Key(x => x.HouseSize, KeyType.Text)
             .CreateAsync();
 
-        // Check if data exists within the table
-        var count = await DB.CountAsync<Item>();
+        // Create a scope to get the services within the application
+        using var scope = app.Services.CreateScope();
 
-        // For an empty table
-        if (count == 0)
-        {
-            Console.WriteLine("No data - will attempt to seed");
-            //  Read JSON data from the stored JSON file
-            var itemData = await File.ReadAllTextAsync("Data/rents.json");
+        // Get the RentServiceHttpClient service
+        var httpClient = scope.ServiceProvider
+            .GetRequiredService<RentServiceHttpClient>();
 
-            // State options for deserializing to be case insensitive
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        // Get the items from the Rent service (only untracked updated ones)
+        var items = await httpClient.GetItemsForSearchDb();
 
-            // Convert JSON data to a list collection of items
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        // TODO: Print the number of items returned. Remove when no longer necessary
+        Console.WriteLine(items.Count + " returned from the Rent Service");
 
-            // Save items data into the database
-            await DB.SaveAsync(items);
-        }
+        // Save the items to the database if there are any
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
+
