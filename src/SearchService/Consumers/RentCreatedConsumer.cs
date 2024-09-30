bc0283449121d0f72fac2726842d803b50fbca1a
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-09-30 23:10:28 +0600

    feat: create the rent created consumer

---
+++ src/SearchService/Consumers/RentCreatedConsumer.cs
@@ -0,0 +1,39 @@
+using System;
+using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+/// <summary>
+/// A consumer for the <see cref="RentCreated"/> message.
+/// </summary>
+public class RentCreatedConsumer : IConsumer<RentCreated>
+{
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RentCreatedConsumer"/> class.
+    /// </summary>
+    /// <param name="mapper">The AutoMapper instance to use for mapping between objects.</param>
+    public RentCreatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    /// Consumes the specified message and saves it to the database
+    /// </summary>
+    /// <param name="context">The message context.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task Consume(ConsumeContext<RentCreated> context)
+    {
+        Console.WriteLine("--> Consuming rent created: " + context.Message.Id);
+
+        var item = _mapper.Map<Item>(context.Message);
+        await item.SaveAsync();
+    }
+}
+
