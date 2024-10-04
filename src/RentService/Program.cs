6e694ae46ff97ae5d2c03b44dce57377f52913ef
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-10-04 20:43:49 +0600

    feat: add Mass Transit Entity Framework Core service to the Rent Service

--- src/RentService/Program.cs
+++ src/RentService/Program.cs
@@ -13,10 +13,29 @@ builder.Services.AddDbContext<RentDbContext>(opt =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit(x =>
 {
+    /*************  ✨ Codeium Command 🌟  *************/
+    // Set up the Entity Framework Outbox which is used to store messages in the database
+    // that are yet to be sent. This is useful if the application is shut down before all
+    // messages can be sent.
+    x.AddEntityFrameworkOutbox<RentDbContext>(o =>
+    {
+        // Delay each message in the outbox by 10 seconds before sending it
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        // Use Postgres as the database provider
+        o.UsePostgres();
+
+        // Use the outbox as the bus
+        o.UseBusOutbox();
+    });
+
+    // Set up the RabbitMQ message bus
     x.UsingRabbitMq((context, cfg) =>
     {
+        // Configure the endpoints for the RabbitMQ message bus
         cfg.ConfigureEndpoints(context);
     });
+    /******  17467043-c828-474d-9ca2-b665bf283724  *******/
 });
 
 var app = builder.Build();
