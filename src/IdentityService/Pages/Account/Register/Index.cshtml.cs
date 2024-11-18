4927af5353e1abbdfb954bbaf9da134ed1b46980
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-11-18 23:15:23 +0600

    feat: add POST request implementation for form submission on the Register page

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -33,4 +33,39 @@ public class Index : PageModel
 
         return Page();
     }
+
+    public async Task<IActionResult> OnPost()
+    {
+        // Triggered from a cancel button therefore redirected to home page
+        if (Input.Button != "register") return Redirect("~/");
+        
+        // Model state needs to be validated for frontend clients
+        if (ModelState.IsValid)
+        {
+            // Create new Application User with defaults initialized
+            var user = new ApplicationUser
+            {
+                UserName = Input.Username,
+                Email = Input.Email,
+                EmailConfirmed = true
+            };
+            
+            // Create the user
+            var result = await _userManager.CreateAsync(user, Input.Password);
+
+            // Add full name from input to the user claims
+            if (result.Succeeded)
+            {
+                await _userManager.AddClaimsAsync(user, new Claim[]
+                {
+                    new Claim(JwtClaimTypes.Name, Input.FullName)
+                });
+
+                RegisterSuccess = true;
+            }
+
+        }
+        
+        return Page();
+    }
 }
